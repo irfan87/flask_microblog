80a09176090793aae9779beac4a57bf991fc1e72
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-10-06 15:09:44 +0800

    setup templates and applied dummy data to the templates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 # define routes
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world!"
+    # try call user's list
+    user = {"username": "irfan shukri"}
+
+    # fake post
+    posts = [
+        {"author": {"username": "John"}, "body": "test post1"},
+        {"author": {"username": "Jane"}, "body": "test post2"},
+        {"author": {"username": "Samad"}, "body": "test post3"},
+        {"author": {"username": "irfan shukri"}, "body": "test post4"},
+        {"author": {"username": "wok yoh"}, "body": "test post5"},
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
